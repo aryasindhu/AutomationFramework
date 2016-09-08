70e325ccb2ba2bc0eea2c51cf4c6d4f31a4e3599
aryasindhusahu <aryasindhusahu@192.168.10.107>
2016-09-08 21:51:21 +0530

    Initial Code Commit

---
+++ src/main/java/com/mentorstudies/automationframework/TestRunner.java
@@ -0,0 +1,16 @@
+package com.mentorstudies.automationframework;
+
+/**
+ * 
+ * @author aryasindhusahu
+ *
+ */
+public class TestRunner {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi All, Its working !!");
+		
+	}
+
+}
